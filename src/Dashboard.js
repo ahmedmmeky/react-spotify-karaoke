255f7da536ea8ec3154f17a063da504d3060586d
ahmedmmeky <meky.wrk@gmail.com>
2021-08-28 15:13:48 -0400

    was able to properly use the spotify api on the client side to search for tracks and get the artist, title, uri, and album cover for each of the tracks; right now I have access to this data properly in my console but I still need to display it on the page.

---
+++ src/Dashboard.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { Container, Form } from "react-bootstrap";
+import useAuth from "./useAuth";
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "561a0c64ba91427f8ba2ba70e2982070",
+});
+
+export default function Dashboard({ code }) {
+  const accessToken = useAuth(code);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!search) return;
+    if (!accessToken) return;
+    let cancel = false;
+    spotifyApi.searchTracks(search).then((res) => {
+      if (cancel) return;
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImg = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image;
+              return smallest;
+            },
+            track.album.images[0]
+          );
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImg.url,
+          };
+        })
+      );
+    });
+
+    return () => (cancel = true);
+  }, [search, accessToken]);
+  return (
+    <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
+      <Form.Control
+        type="search"
+        placeholder="Search Songs/Artists"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+        Songs
+      </div>
+      <div>Bottom</div>
+    </Container>
+  );
+}
---
+++ src/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Container } from "react-bootstrap";
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=561a0c64ba91427f8ba2ba70e2982070&response_type=code&redirect_uri=http://localhost:3000/login&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+
+export default function Login() {
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <a href={AUTH_URL} className="btn btn-success btn-lg">
+        Login With Spotify
+      </a>
+    </Container>
+  );
+}
