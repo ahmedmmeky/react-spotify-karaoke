e784fb86c9558b9a2e94b611ad85a6067fbc2a9e
ahmedmmeky <meky.wrk@gmail.com>
2021-08-30 11:42:18 -0400

    trying to deploy.

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { Container, Form } from "react-bootstrap";
 import useAuth from "./useAuth";
 import SpotifyWebApi from "spotify-web-api-node";
+import TrackSearchResult from "./TrackSearchResult";
+import Player from "./Player";
+import axios from "axios";
 
 const spotifyApi = new SpotifyWebApi({
   clientId: "561a0c64ba91427f8ba2ba70e2982070",
@@ -11,6 +14,30 @@ export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [playingTrack, setPlayingTrack] = useState();
+  const [lyrics, setLyrics] = useState("");
+
+  function chooseTrack(track) {
+    setPlayingTrack(track);
+    setSearch("");
+    setLyrics("");
+  }
+
+  useEffect(() => {
+    if (!playingTrack) return;
+
+    axios
+      .get("http://localhost:3001/lyrics", {
+        params: {
+          track: playingTrack.title,
+          artist: playingTrack.artist,
+        },
+      })
+      .then((res) => {
+        console.log(res.data.lyrics);
+        setLyrics(res.data.lyrics);
+      });
+  }, [playingTrack]);
 
   useEffect(() => {
     if (!accessToken) return;
@@ -18,7 +45,7 @@ export default function Dashboard({ code }) {
   }, [accessToken]);
 
   useEffect(() => {
-    if (!search) return;
+    if (!search) return setSearchResults([]);
     if (!accessToken) return;
     let cancel = false;
     spotifyApi.searchTracks(search).then((res) => {
@@ -53,9 +80,22 @@ export default function Dashboard({ code }) {
         onChange={(e) => setSearch(e.target.value)}
       />
       <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
-        Songs
+        {searchResults.map((track) => (
+          <TrackSearchResult
+            track={track}
+            key={track.uri}
+            chooseTrack={chooseTrack}
+          />
+        ))}
+        {searchResults.length === 0 && (
+          <div className="text-center" style={{ whiteSpace: "pre" }}>
+            {lyrics}
+          </div>
+        )}
       </div>
-      <div>Bottom</div>
+      <div>
+        <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+      </div>
     </Container>
   );
 }
