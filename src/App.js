f8643e909d4679e9febe75d78d7f6e87425570f8
ahmedmmeky <meky.wrk@gmail.com>
2021-08-31 23:40:44 -0400

    deploying full client.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Dashboard from "./Dashboard";
 const code = new URLSearchParams(window.location.search).get("code");
 
 function App() {
+  fetch("https://react-karaoke-v3.herokuapp.com/")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
   return code ? <Dashboard code={code} /> : <Login />;
 }
 
--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Container } from "react-bootstrap";
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=561a0c64ba91427f8ba2ba70e2982070&response_type=code&redirect_uri=https://react-karaoke-v2.herokuapp.com/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+  "https://accounts.spotify.com/authorize?client_id=561a0c64ba91427f8ba2ba70e2982070&response_type=code&redirect_uri=https://itskaraoketime.com/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
   return (
